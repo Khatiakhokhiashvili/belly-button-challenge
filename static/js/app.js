b2651fa5c51b8825e283f9a4a210e98390f76d38
Khatiakhokhiasvhili <Khatia.khokhiashvili@gmail.com>
2024-06-09 23:10:58 -0400

    work in progress

--- static/js/app.js
+++ static/js/app.js
@@ -3,20 +3,26 @@ function buildMetadata(sample) {
   d3.json("https://static.bc-edx.com/data/dl-1-2/m14/lms/starter/samples.json").then((data) => {
 
     // get the metadata field
+    const metadata = data.metadata;
 
 
     // Filter the metadata for the object with the desired sample number
+    const result = metadata.filter(obj => obj.id == sample)[0];
 
 
     // Use d3 to select the panel with id of `#sample-metadata`
+    const panel = d3.select("#sample-metadata");
 
 
     // Use `.html("") to clear any existing metadata
 
+    panel.html("");
 
     // Inside a loop, you will need to use d3 to append new
     // tags for each key-value in the filtered metadata.
-
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    
   });
 }
 
@@ -26,28 +32,63 @@ function buildCharts(sample) {
 
     // Get the samples field
 
+    const samples = data.samples;
 
     // Filter the samples for the object with the desired sample number
 
+    const result = samples.filter(obj => obj.id == sample)[0];
 
     // Get the otu_ids, otu_labels, and sample_values
-
+    const otu_ids = result.otu_ids;
+    const otu_labels = result.otu_labels;
+    const sample_values = result.sample_values
 
     // Build a Bubble Chart
-
+    const bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {
+        size: sample_values,
+        color: otu_ids,
+        colorscale: "Earth"
+      }
+    }];
 
     // Render the Bubble Chart
+    const bubbleLayout = {
+      title: "OTU Samples",
+      margin: { t: 0 },
+      hovermode: "closest",
+      xaxis: { title: "OTU ID" },
+      margin: { t: 30 }
+    };
 
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
     // For the Bar Chart, map the otu_ids to a list of strings for your yticks
+    const yticks = otu_ids.slice(0, 10).map(id => `OTU ${id}`).reverse();
 
 
     // Build a Bar Chart
+    const barData = [{
+      y: yticks,
+      x: sample_values.slice(0, 10).reverse(),
+      text: otu_labels.slice(0, 10).reverse(),
+      type: "bar",
+      orientation: "h"
+    }];
+
     // Don't forget to slice and reverse the input data appropriately
 
 
     // Render the Bar Chart
+    const barLayout = {
+      title: "Top 10 OTUs Found in Sample"
+    };
 
+    Plotly.newPlot("bar", barData, barLayout);
   });
 }
 
@@ -57,26 +98,32 @@ function init() {
 
     // Get the names field
 
+  const names = data.names;
 
     // Use d3 to select the dropdown with id of `#selDataset`
+    const selector = d3.select("#selDataset");
 
 
     // Use the list of sample names to populate the select options
     // Hint: Inside a loop, you will need to use d3 to append a new
     // option for each sample name.
-
+  names.forEach((sample) => {selector.append("option").text(sample).property("value", sample);
+    });
 
     // Get the first sample from the list
 
-
+  const firstSample = names[0];
     // Build charts and metadata panel with the first sample
-
+  buildCharts(firstSample);
+  buildMetadata(firstSample);
   });
 }
 
 // Function for event listener
 function optionChanged(newSample) {
   // Build charts and metadata panel each time a new sample is selected
+  buildCharts(newSample);
+  buildMetadata(newSample);
 
 }
 
